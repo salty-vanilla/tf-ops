c40e335365e627a23ee9418dfdc75407a24e21df
Shunsuke NAKATSUKA <jacketgardensio@gmail.com>
2019-12-30 11:22:04 +0900

    Update conv.py

--- blocks/conv.py
+++ blocks/conv.py
@@ -92,3 +92,59 @@ class ConvBlock(tf.keras.Model):
             x = self.norm(x, training=training)
         x = activation(x, self.act)
         return x
+
+
+class ResidualBlock(tf.keras.Model):
+    def __init__(self, filters,
+                 kernel_size=(3, 3),
+                 activation_=None,
+                 dilation_rate=(1, 1),
+                 sampling='same',
+                 normalization=None,
+                 spectral_norm=False,
+                 lr_equalization=False,
+                 **conv_params):
+        super().__init__()
+        self.filters = filters
+        self.sampling = sampling
+        self.conv1 = ConvBlock(filters,
+                               kernel_size,
+                               activation_,
+                               dilation_rate,
+                               'same',
+                               normalization,
+                               spectral_norm,
+                               lr_equalization,
+                               **conv_params)
+        self.conv2 = ConvBlock(filters,
+                               kernel_size,
+                               None,
+                               dilation_rate,
+                               sampling,
+                               normalization,
+                               spectral_norm,
+                               lr_equalization,
+                               **conv_params)
+        self.act = activation_
+        self.shortcut_conv = tf.keras.layers.Conv2D(filters, (1, 1))
+
+    def call(self, inputs,
+             training=None,
+             mask=None):
+        x = self.conv1(inputs, training=training)
+        x = self.conv2(x, training=training)
+
+        if inputs.get_shape().as_list()[-1] != self.filters:
+            x += self.shortcut(self.shortcut_conv(inputs))
+        else:
+            x += self.shortcut(inputs)
+        x = activation(x, self.act)
+        return x
+
+    def shortcut(self, x):
+        if self.sampling in ['deconv', 'up', 'subpixel']:
+            return tf.keras.layers.UpSampling2D()(x)
+        elif self.sampling in ['max_pool', 'avg_pool', 'stride']:
+            return tf.keras.layers.AveragePooling2D()(x)
+        else:
+            return x
