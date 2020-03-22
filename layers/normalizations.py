434410f9c6346a9ed9ee80676931b97e81800888
salty-vanilla <jacketgardensio@gmail.com>
2020-03-22 06:23:16 +0000

    [Update] add param center and scale

--- layers/normalizations.py
+++ layers/normalizations.py
@@ -17,28 +17,38 @@ class PixelNorm(tf.keras.layers.Layer):
 
 
 class InstanceNorm(tf.keras.layers.Layer):
-    def __init__(self, beta_initializer='zeros',
+    def __init__(self, center=True,
+                 scale=True,
+                 beta_initializer='zeros',
                  gamma_initializer='ones'):
         super().__init__()
+        self.center = center
+        self.scale = scale
         self.beta_initializer = beta_initializer
         self.gamma_initializer = gamma_initializer
 
     def build(self, input_shape):
-        self.gamma = self.add_weight(shape=(input_shape[-1]),
-                                     name='gamma',
-                                     initializer=self.gamma_initializer)
-        self.beta = self.add_weight(shape=(input_shape[-1]),
-                                    name='beta',
-                                    initializer=self.beta_initializer)
+        if self.scale:
+            self.gamma = self.add_weight(shape=(input_shape[-1]),
+                                        name='gamma',
+                                        initializer=self.gamma_initializer)
+        if self.center:
+            self.beta = self.add_weight(shape=(input_shape[-1]),
+                                        name='beta',
+                                        initializer=self.beta_initializer)
 
     def call(self, inputs, **kwargs):
         x = inputs
         mean, var = tf.nn.moments(x, [1, 2], keepdims=True)
-        return tf.nn.batch_normalization(x,
+
+        gamma = self.gamma if hasattr(self, 'gamma') else tf.constant(1.)
+        beta = self.beta if hasattr(self, 'beta') else tf.constant(0.)
+
+        return tf.nn.batch_normalization(x, 
                                          mean,
                                          var,
-                                         offset=self.beta,
-                                         scale=self.gamma,
+                                         offset=beta,
+                                         scale=gamma,
                                          variance_epsilon=K.epsilon())
 
     def compute_output_shape(self, input_shape):
@@ -46,11 +56,15 @@ class InstanceNorm(tf.keras.layers.Layer):
 
 
 class LayerNorm(tf.keras.layers.Layer):
-    def __init__(self, begin_norm_axis=1,
+    def __init__(self, center=True,
+                 scale=True,
+                 begin_norm_axis=1,
                  begin_params_axis=-1,
                  beta_initializer='zeros',
                  gamma_initializer='ones'):
         super().__init__()
+        self.center = center
+        self.scale = scale
         self.begin_norm_axis = begin_norm_axis
         self.begin_param_axis = begin_params_axis
         self.beta_initializer = beta_initializer
@@ -61,23 +75,28 @@ class LayerNorm(tf.keras.layers.Layer):
         self.input_rank = input_shape.ndims
         if self.begin_norm_axis < 0:
             self.begin_norm_axis = self.input_rank + self.begin_norm_axis
-
-        self.gamma = self.add_weight(shape=params_shape,
-                                     name='gamma',
-                                     initializer=self.gamma_initializer)
-        self.beta = self.add_weight(shape=params_shape,
-                                    name='beta',
-                                    initializer=self.beta_initializer)
+        if self.scale:
+            self.gamma = self.add_weight(shape=params_shape,
+                                        name='gamma',
+                                        initializer=self.gamma_initializer)
+        if self.center:
+            self.beta = self.add_weight(shape=params_shape,
+                                        name='beta',
+                                        initializer=self.beta_initializer)
 
     def call(self, inputs, **kwargs):
         x = inputs
         norm_axes = list(range(self.begin_norm_axis, self.input_rank))
         mean, var = tf.nn.moments(inputs, norm_axes, keepdims=True)
+
+        gamma = self.gamma if hasattr(self, 'gamma') else tf.constant(1.)
+        beta = self.beta if hasattr(self, 'beta') else tf.constant(0.)
+
         return tf.nn.batch_normalization(x,
                                          mean,
                                          var,
-                                         offset=self.beta,
-                                         scale=self.gamma,
+                                         offset=beta,
+                                         scale=gamma,
                                          variance_epsilon=K.epsilon())
 
     def compute_output_shape(self, input_shape):
